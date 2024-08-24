0981665d63904e202cdca6baa07442f8fb024aa3
JaimeNarvaez <jaimeangel936@gmail.com>
2024-08-23 20:24:30 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    print("Lo lamento, esta palabra no se encuentra en nuestro diccionario")
+    # ¿Qué hacer si no se encuentra la palabra?
